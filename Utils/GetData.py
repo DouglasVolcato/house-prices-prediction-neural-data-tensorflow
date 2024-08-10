efab69619bfc5563474692cb7898f35de581f1dc
DouglasVolcato <douglasvolcato@gmail.com>
2024-08-10 18:57:09 -0300

    refactor: remove unused lines

--- Utils/GetData.py
+++ Utils/GetData.py
@@ -4,16 +4,6 @@ class GetData:
     def __init__(self):
         self.__csv_file_path = 'data/housing.csv'
 
-    # housing_median_age
-    # total_rooms
-    # total_bedrooms
-    # median_income
-    # median_house_value
-    # ocean_proximity_<1H OCEAN
-    # ocean_proximity_INLAND	
-    # ocean_proximity_ISLAND	
-    # ocean_proximity_NEAR BAY	
-    # ocean_proximity_NEAR OCEAN
     def execute(self):
         data = pd.read_csv(self.__csv_file_path)
         data = pd.get_dummies(data, columns=['ocean_proximity'])
